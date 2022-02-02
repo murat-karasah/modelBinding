b579ddf23834092751c2330cbc7a2f2ccfa9d976
Murat Karasah <mkarasah@gmail.com>
2022-02-02 11:03:20 +0300

    Add project files.

---
+++ modelBinding/Controllers/userController.cs
@@ -0,0 +1,42 @@
+﻿using modelBinding.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace modelBinding.Controllers
+{
+    public class userController : Controller
+    {
+        kullaniciEntities db = new kullaniciEntities();
+        // GET: user
+        public ActionResult Index()
+        {
+            var model = new userViewIndexModel
+            {
+                Users = db.usertablo.ToList()
+
+            };
+            return View(model);
+        }
+
+        public ActionResult Insert()
+        {
+            return View();
+        }
+
+        #region Using Request Object
+        [HttpPost]
+        public RedirectToRouteResult UserInsert()
+        {
+            var firstname = Request.Form["firstname"];
+            var lastname = Request.Form["lastname"];
+            var birthdate = Convert.ToDateTime(Request.Form["birtdate"]);
+            db.usertablo.Add(new usertablo { firstname=firstname,lastname=lastname,birtdate=birthdate });
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        #endregion
+    }
+}
---
+++ modelBinding/Models/userViewIndexModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace modelBinding.Models
+{
+    public class userViewIndexModel
+    {
+        public List<usertablo> Users  { get; set; }
+    }
+}
